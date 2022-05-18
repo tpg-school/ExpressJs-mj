1006456a72459e934b83ecae98a097de7d5b46dc
Mario <mario.ja20@gmail.com>
2022-05-18 14:20:44 -0500

    Add /Datos

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Capacitacion Git')
 })
 
+app.get('/datos', (req, res) => {
+    res.send('Datos Git')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
